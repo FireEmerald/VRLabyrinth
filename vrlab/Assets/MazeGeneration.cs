0d5d73b88bd823a868c079fc06bb204fd0f418f6
Pascal Stech <pascal@stech.at>
2019-07-23 19:50:09 +0200

    Auto placement of walls and start position

---
+++ vrlab/Assets/MazeGeneration.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGeneration : MonoBehaviour
+{
+    public GameObject wall;
+    public GameObject cameraRig;
+
+    private char[,] maze = new char[,]
+    {
+        {'W','W','W','W','W','W','W','W','W','W','W'},
+        {'W','_','_','_','_','_','_','_','W','_','W'},
+        {'W','_','S','_','W','W','W','_','_','_','W'},
+        {'W','_','_','_','W','_','W','_','W','W','W'},
+        {'W','_','W','W','W','_','_','_','W','_','W'},
+        {'W','_','_','W','W','W','W','W','W','_','W'},
+        {'W','W','_','W','W','_','_','_','_','_','W'},
+        {'W','W','_','W','W','_','W','W','_','W','W'},
+        {'W','_','_','_','_','_','W','W','_','_','W'},
+        {'W','W','W','W','W','W','W','W','W','_','W'}
+    };
+
+    void Start()
+    {
+        for (int x = 0; x < maze.GetLength(0); x++)
+        {
+            for (int y = 0; y < maze.GetLength(1); y++)
+            {
+                char type = maze[x, y];
+
+                switch (type) {
+                    case 'W':
+                        Instantiate(wall, new Vector3(x, 1, y), Quaternion.identity);
+                        break;
+                    case 'S':
+                        cameraRig.transform.position = new Vector3(x, 0, y);
+                        break;
+                }
+                
+
+            }
+        }
+    }
+}
